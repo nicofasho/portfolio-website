2225e60069ceb62bc29d2ca54b2967200934f9c2
nicofasho <38410585+nicofasho@users.noreply.github.com>
2018-07-23 10:31:59 -0700

    Updated scripts, added interactivity to home page

--- js/sripts.js
+++ js/sripts.js
@@ -1,1 +1,15 @@
 console.log("Hey! Thanks for visiting my portfolio!");
+// $(document).ready(function () {
+//     $(".project-preview").on("click", function () {
+//         $(this).siblings(".project-details").slideToggle();
+//     });
+// });
+
+$(document).ready(function(){
+    $(".project").on("mouseenter", function(){
+        $(this).find(".project-details").fadeIn();
+    });
+    $(".project").on("mouseleave", function(){
+        $(this).find(".project-details").fadeOut();
+    });
+});
