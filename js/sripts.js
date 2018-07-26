61c0ee3f0f29483050092e2a28612ae692d92469
nicofasho <38410585+nicofasho@users.noreply.github.com>
2018-07-26 12:42:58 -0700

    Commented out js, added css hover effect, added phlash project page

--- js/sripts.js
+++ js/sripts.js
@@ -5,11 +5,11 @@ console.log("Hey! Thanks for visiting my portfolio!");
 //     });
 // });
 
-$(document).ready(function(){
-    $(".project").on("mouseenter", function(){
-        $(this).find(".project-details").fadeIn();
-    });
-    $(".project").on("mouseleave", function(){
-        $(this).find(".project-details").fadeOut();
-    });
-});
+// $(document).ready(function(){
+//     $(".project").on("mouseenter", function(){
+//         $(this).find(".project-details").fadeIn();
+//     });
+//     $(".project").on("mouseleave", function(){
+//         $(this).find(".project-details").fadeOut();
+//     });
+// });
